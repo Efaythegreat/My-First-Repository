6922b351bb3ed6bea537fd96160da67379beee24
Efay <65887283+Efaythegreat@users.noreply.github.com>
2021-11-17 17:28:27 -0800

    added img, changed bg-color

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>How's Life</h1>
+      <h1>Get Rick Rolled</h1>
       <h2>Year: 2021, HS: Balboa</h2>
+      <img src="https://c.tenor.com/u9XnPveDa9AAAAAM/rick-rickroll.gif" />
     </div>
   );
 }
