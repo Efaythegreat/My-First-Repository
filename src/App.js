597498778c08a631bdec063a5dad37a34074fdad
Efay <65887283+Efaythegreat@users.noreply.github.com>
2021-11-17 16:34:34 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>How's Life</h1>
+      <h2>Sometimes I feel alive</h2>
+    </div>
+  );
+}
